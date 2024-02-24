75c4c9b2557182f927bd75b60a6d85cf1a47ac3e
Anoryike <149113497+Anoryike@users.noreply.github.com>
2024-02-24 02:30:46 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// Напишіть такий JavaScript, щоб після натискання на кнопку button, елемент <div id="text"> зникав.
+// const button = document.getElementById("btn");
+// const text = document.getElementById("text");
+
+// button.addEventListener("click", () => (text.style.display = "none"));
+
+// Напишіть такий код, щоб після натискання на кнопку, вона зникала
+// const button = document.getElementById("btn");
+// button.addEventListener("click", () => (button.style.display = "none"));
+
+// Створіть дерево, яке показує/приховує дочірні вузли при кліці
+// var parents = document.querySelectorAll(".parent");
+
+// for (var i = 0; i < parents.length; i++) {
+//   parents[i].addEventListener("click", function () {
+//     var child = this.querySelector(".child");
+//     child.style.display = child.style.display === "none" ? "block" : "none";
+//   });
+// }
